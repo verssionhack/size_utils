de19cd37b6929b1ea9ddcfcba357d73f5737b8fd
yinpeach <yinpeach0314@gmail.com>
2024-10-31 16:33:27 +0800

    update functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,8 @@ mod tests {
     use super::Size;
     #[test]
     fn it_works() {
-        let mut a = Size::from_byte(1024usize);
-        let mut b = Size::from_byte(512usize);
+        let mut a = Size::from_byte(1024);
+        let mut b = Size::from_byte(512);
         println!("{:?} + {:?} = {:?}", a, b, a + b);
         println!("{:?} - {:?} = {:?}", a, b, a - b);
         a += b;
--- src/type.rs
+++ src/type.rs
@@ -3,7 +3,7 @@ use std::ops::{Add, AddAssign, Deref, DerefMut, Sub, SubAssign};
 
 #[derive(Debug, Default, Copy, Clone, PartialEq, PartialOrd, Hash, Eq)]
 pub struct Size {
-    byte: u64,
+    _byte: u64,
 }
 
 impl Serialize for Size {
@@ -26,35 +26,22 @@ impl<'de> Deserialize<'de> for Size {
 
 impl AddAssign for Size {
     fn add_assign(&mut self, other: Self) {
-        *self = Self::from_byte(self.as_byte() + other.as_byte());
-    }
-}
-
-impl Deref for Size {
-    type Target = u64;
-    fn deref(&self) -> &Self::Target {
-        &self.byte
-    }
-}
-
-impl DerefMut for Size {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.byte
+        self._byte += other._byte;
     }
 }
 
 impl Add for Size {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self::from_byte(self.as_byte() + other.as_byte())
+        Self::from_byte(self._byte + other._byte)
     }
 }
 
 impl Sub for Size {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
-        Self::from_byte(if self.as_byte() > other.as_byte() {
-            self.as_byte() - other.as_byte()
+        Self::from_byte(if self._byte > other._byte {
+            self._byte - other._byte
         } else {
             0
         })
@@ -76,76 +63,79 @@ where
     T: Into<u64>,
 {
     fn from(value: T) -> Self {
-        Self { byte: value.into() }
+        Self { _byte: value.into() }
     }
 }
 
 impl Size {
-    pub fn set_byte(&mut self, byte: impl Into<u64>) {
-        self.byte = byte.into();
+    pub fn set_byte(&mut self, value: u64) {
+        self._byte = value;
     }
-    pub fn reset(&mut self) {
-        self.byte = 0;
+    pub fn bzero(&mut self) {
+        self._byte = 0;
     }
-    pub fn from_byte(byte: impl Into<u64>) -> Self {
-        Self { byte: byte.into() }
+    pub const fn from_byte(value: u64) -> Self {
+        Self { _byte: value }
     }
-    pub fn from_kb(byte: impl Into<u64>) -> Self {
+    pub const fn from_kb(value: u64) -> Self {
         Self {
-            byte: byte.into() * 1024,
+            _byte: value * 1024,
         }
     }
-    pub fn from_mb(byte: impl Into<u64>) -> Self {
+    pub const fn from_mb(value: u64) -> Self {
         Self {
-            byte: byte.into() * 1024,
+            _byte: value * 1024,
         }
     }
-    pub fn from_gb(byte: impl Into<u64>) -> Self {
+    pub const fn from_gb(value: u64) -> Self {
         Self {
-            byte: byte.into() * 1024 * 1024,
+            _byte: value * 1024 * 1024,
         }
     }
-    pub fn from_pb(byte: impl Into<u64>) -> Self {
+    pub const fn from_pb(value: u64) -> Self {
         Self {
-            byte: byte.into() * 1024 * 1024 * 1024,
+            _byte: value * 1024 * 1024 * 1024,
         }
     }
-    pub fn from_eb(byte: impl Into<u64>) -> Self {
+    pub const fn from_eb(value: u64) -> Self {
         Self {
-            byte: byte.into() * 1024 * 1024 * 1024 * 1024,
+            _byte: value * 1024 * 1024 * 1024 * 1024,
         }
     }
-    pub fn as_byte(&self) -> u64 {
-        self.byte
+    pub const fn as_byte(&self) -> u64 {
+        self._byte
+    }
+    pub const fn as_byte_f64(&self) -> f64 {
+        self._byte as f64
     }
     pub fn as_kb_f64(&self) -> f64 {
-        self.byte as f64 / 1024f64
+        self.as_byte_f64() / 1024f64
     }
-    pub fn as_kb(&self) -> u64 {
-        self.byte / 1024
+    pub const fn as_kb(&self) -> u64 {
+        self.as_byte() / 1024
     }
     pub fn as_mb_f64(&self) -> f64 {
         self.as_kb_f64() / 1024f64
     }
-    pub fn as_mb(&self) -> u64 {
-        self.byte / 1024 / 1024
+    pub const fn as_mb(&self) -> u64 {
+        self.as_kb() / 1024
     }
     pub fn as_gb_f64(&self) -> f64 {
         self.as_mb_f64() / 1024f64
     }
-    pub fn as_gb(&self) -> u64 {
-        self.byte / 1024 / 1024 / 1024
+    pub const fn as_gb(&self) -> u64 {
+        self.as_mb() / 1024
     }
     pub fn as_pb_f64(&self) -> f64 {
         self.as_gb_f64() / 1024f64
     }
-    pub fn as_pb(&self) -> u64 {
-        self.byte / 1024 / 1024 / 1024 / 1024
+    pub const fn as_pb(&self) -> u64 {
+        self.as_gb() / 1024
     }
     pub fn as_eb_f64(&self) -> f64 {
         self.as_gb_f64() / 1024f64
     }
-    pub fn as_eb(&self) -> u64 {
-        self.byte / 1024 / 1024 / 1024 / 1024 / 1024
+    pub const fn as_eb(&self) -> u64 {
+        self.as_pb() / 1024
     }
 }
